128046fd88a1ff30568df03f2173d4a42aef01ce
Marco Comuzzi <Marco Comuzzi>
2017-01-17 12:12:30 +0900

    some progress on exact path enumeration

--- opsupport_bpm/models/bf_path_enumerator.py
+++ opsupport_bpm/models/bf_path_enumerator.py
@@ -10,10 +10,26 @@ from opsupport_bpm.aco.aco_directed_hypergraph import aco_search_nonrec
 from opsupport_bpm.models.hypergraph import reset_pheromone
 
 import random
+from itertools import permutations, combinations
+
 
 class Node:
     def __init__(self):
         self.data = []
+        self.parent = None
+        self.children = []
+        self.level = 0
+        self.is_merge = False
+    def set_level(self,l):
+        self.level = l
+    def set_parent(self, parent):
+        self.parent = parent
+    def is_leaf(self):
+        return self.children == [] or self.is_merge
+    def set_children(self):
+        for d in self.data:
+            if type(d) is not str:
+                self.children.append(d)
     def add_activity(self, act_name):
         self.data.append(act_name)
     def print_node(self, level):
@@ -21,7 +37,7 @@ class Node:
             if type(act) is not str:
                 act.print_node(level + 1)
             else:
-                print("..."*level +act)
+                print("..."*level +act+" "+str(self.level))
 
 
 
@@ -54,7 +70,7 @@ class BF_PathEnumerator():
                 end_node = node
         #hypertree = Node()
         self.explore(hypertree, start_node, end_node)
-        return hypertree.data
+        return hypertree
 
     def explore(self, tree, node_start, node_end):
         current_node = node_start
@@ -67,6 +83,8 @@ class BF_PathEnumerator():
                 next_nodes = self.opt_hgr.get_hyperedge_head(list(self.opt_hgr.get_forward_star(current_node))[0])
                 for node in next_nodes:
                     new_block = Node()
+                    new_block.set_parent(tree)
+                    new_block.set_level(new_block.parent.level + 1)
                     tree.data.append(new_block)
                     end_node = "tau join"+id
                     self.explore(new_block, node, end_node)
@@ -80,49 +98,145 @@ class BF_PathEnumerator():
         tree.data.append(current_node)
         # take care of last node
 
-    def enumerate_paths(self, tree, num_ants, index):
-        paths = {}
-        start_node = tree.data[0]
-        end_node = tree.data[len(tree.data)-1]
-        for i in range(num_ants):
-            paths[i] = []
-            index, data_index = 0, 0
-            self.ant_walk(tree.data, paths[i], index, 0, start_node, end_node)
-        return paths
 
 
-    def ant_walk(self, data, path, index, data_index, start_node, end_node):
-        current_node = start_node
-        while current_node != end_node:
-            if current_node[:9] == "tau split":
-                id = current_node[9]
-                this_node = current_node
-                path_temp = []
-                while (type(this_node) is not str) or (this_node[:9] != "tau join"):
-                    path_temp.append(this_node)
-                    data_index += 1
-                    this_node = data[data_index]
-                path_temp.pop(0)
-                path_temp.pop(len(path_temp)-1)
-                next = random.choice[path_temp]
-                self.ant_walk(next.data, path, index, data_index, current_node, "tau join"+id)
-            elif type(current_node) is not str:
-                data_index += 1
-                current_node = data[data_index]
+    def vertical_merge(self, data_up, data_down):
+        """
+        does teh vertical merge step: tested!
+        :param data_up: a list with tau split/join
+        :param data_down: a "leaf" list (that is, without tau split/join)
+        :return:
+        """
+        # get all the prmutations of data_down
+        perm_down = permutations(data_down)
+
+        # find the sublist between tau split/join in data_up
+        for value in data_up:
+            if value[0:9] == "tau split":
+                sublist_start = data_up.index(value)
+            if value[0:8] == "tau join":
+                sublist_end = data_up.index(value)
+
+        # do the merge (create list of lists)
+        merge = []
+        data_up[sublist_start:sublist_end+1] = next(perm_down)
+        copy = self.deep_copy(data_up)
+        merge.append(copy)
+
+        for perm in perm_down:
+            data_up[sublist_start:sublist_start+len(perm)] = perm
+            copy = self.deep_copy(data_up)
+            merge.append(copy)
+
+
+        return merge
+
+
+
+    def deep_copy(self, lst):
+        new = []
+        for l in lst:
+            new.append(l)
+        return new
+
+
+    def horizontal_merge(self, a, b):
+        result = []
+        for lst1 in a:
+            for lst2 in b:
+                result += self.combine(lst1,lst2)
+        return result
+
+    def combine(self, xs, ys):
+        if xs == []: return [ys]
+        if ys == []: return [xs]
+        x, *xs_tail = xs
+        y, *ys_tail = ys
+        return [[x] + l for l in self.combine(xs_tail, ys)] + \
+               [[y] + l for l in self.combine(ys_tail, xs)]
+
+
+    def get_traces(self, hypertree):
+        """
+        Does the traversal of the tree to get all possible traces (doing vertical/horizontal merge as needed)
+
+        The main skeleton is here.
+        However, we need to check for data as list or list of lists (horizontal/vertical integration has to work in all cases)
+        :return:
+        """
+        traces = []
+        if hypertree.is_leaf():
+            if hypertree.parent == None:
+                traces.append(hypertree.data)
+            else:
+                # merge with parent
+                self.vertical_merge(hypertree.parent.data, hypertree.data)
+        else:
+            # check if all children are leaves
+            children = hypertree.children
+            ALL_CHILD = True
+            for child in children:
+                if child.is_leaf() != True:
+                    ALL_CHILD = False
+            if ALL_CHILD:
+                # do horizontal merge of all chidlren
+                # substitute parent with merge
                 pass
             else:
-                path.insert(index,current_node)
-                index += 1
-                data_index += 1
-                current_node = data[data_index]
+                # recursive call on all children
+                pass
+
+
+        pass
+
+
 
 
 if __name__ == '__main__':
-    tree = Node()
-    print(type(tree))
+
     PE = BF_PathEnumerator("popt.hgr")
+
+    # X = ["xxx","tau split0", "0123","0124", "tau join0", "pppp","zzzz"]
+    # Y = ["a", "b", "k"]
+    #
+
+    #
+    # v_merge = PE.vertical_merge(X,Y)
+    # print("========== Vertical merge =============")
+    # print(v_merge)
+    #
+    # Z = [["d","e"]]
+    #
+    # input()
+    # print("========= Horizontal merge =============")
+    #
+    # res = PE.horizontal_merge(v_merge, Z)
+    # print(res)
+    # print(len(res))
+
+
+
+
+
+
+
+
+
+    PE = BF_PathEnumerator("popt.hgr")
+
+    tree = Node()
+    tree.set_parent(None)
     print(PE.get_hyperpath_tree(tree))
+
+    print(tree.parent)
+    tree.set_children()
+    print(tree.is_leaf())
+    print(tree.children)
+    for child in tree.children:
+        print(child.data)
+        print(child.is_leaf())
+
+    #print("========== PRINT TREE ================")
     #tree.print_node(1)
-    paths = PE.enumerate_paths(tree,20, 0)
 
 
